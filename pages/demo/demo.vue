cca4e721bf653e8af4e8804f57da921bafb9610c
zhouhao <18190816904@163.com>
2023-03-05 23:00:12 +0800

    first commit

---
+++ pages/demo/demo.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view>
+			{{total}}
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				unit: 1.5,
+				count: 3
+			}
+		},
+		computed: {
+			total() {
+				return this.unit * this.count
+			}
+		},
+		watch: {	
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<!-- <text class="iconfont icon-smile" style="font-size: 100rpx; color: red;"></text> -->
+		<view style="display: flex;justify-content: center;padding: 50rpx;">
+			<view class="animated faster" hover-class="rubberBand" style='border: 1rpx solid #333333; padding: 20rpx;'>点击这里</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
