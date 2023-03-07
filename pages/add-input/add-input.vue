046871552b14f4bedd271a68ba5aa5b408ee806c
zhouhao <18190816904@163.com>
2023-03-07 22:55:46 +0800

    update

--- pages/add-input/add-input.vue
+++ pages/add-input/add-input.vue
@@ -1,22 +1,56 @@
 <template>
 	<view>
-		add input
+		<!-- 自定义导航 -->
+		<uni-nav-bar left-icon="back" statusBar :border="false">
+			<view class="flex align-center justify-center w-100">
+				所有人可见<text class="iconfont icon-shezhi"></text>
+			</view>
+		</uni-nav-bar>
+		<!-- 文本域 -->
+		<textarea v-model="content" placeholder="说一句话吧" class="uni-textarea px-2" />
+		<!-- 多图上传 -->
+		<upload-image :list="imageList" @change="choose"></upload-image>
+		<!-- 底部操作条 -->
+		<view class="fixed-bottom bg-white flex align-center" style="height: 85rpx;">
+			<view class="iconfont icon-caidan footer-btn animated" hover-class="jello"></view>
+			<view class="iconfont icon-huati footer-btn animated" hover-class="jello"></view>
+			<view class="iconfont icon-tupian footer-btn animated" hover-class="jello"></view>
+			
+			<view class="bg-main text-white ml-auto flex justify-center align-center rounded mr-2 animated" hover-class="jello" style="width: 140rpx;height: 60rpx;">发送</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import UniNavBar from '@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue';
+	import UploadImage from '@/components/common/upload-image.vue';
 	export default {
+		components: {
+			UniNavBar,
+			UploadImage
+		},
 		data() {
 			return {
-				
+				content: "",
+				imageList: []
 			}
 		},
 		methods: {
-			
+			choose(e) {
+				this.imageList = e
+			}
 		}
 	}
 </script>
 
 <style>
+	.footer-btn {
+		width: 86rpx;
+		height: 86rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 50rpx;
+	}
 
 </style>
